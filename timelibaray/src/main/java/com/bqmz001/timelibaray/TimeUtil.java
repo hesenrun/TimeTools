d28b636bcb1a0deadd1ce62f5026c82304247003
bqmz001 <hesenrun919359170@outlook.com>
2020-02-27 15:24:27 +0800

    初次提交

---
+++ timelibaray/src/main/java/com/bqmz001/timelibaray/TimeUtil.java
@@ -0,0 +1,168 @@
+package com.bqmz001.timelibaray;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+
+public class TimeUtil {
+    //获取今年
+    public static synchronized int getNowYear() {
+        return new DateTime().getYear();
+    }
+
+    //获取本月
+    public static synchronized int getNowMonth() {
+        return new DateTime().getMonthOfYear();
+    }
+
+    //获取今天
+    public static synchronized int getNowDay() {
+        return new DateTime().getDayOfMonth();
+    }
+
+    //获取今天的0：00的字符串
+    public static synchronized long getNowDayStartTimeStamp() {
+        return new DateTime()
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    //获取今天的23：59：59.999的字符串
+    public static synchronized long getNowDayEndTimeStamp() {
+        return new DateTime()
+                .withHourOfDay(23)
+                .withMinuteOfHour(59)
+                .withSecondOfMinute(59)
+                .withMillisOfSecond(999)
+                .getMillis();
+    }
+
+    //获取当前时间
+    public static synchronized long getNow() {
+        return new DateTime().getMillis();
+    }
+
+    //获取本月第一天0时
+    public static synchronized long getFirstTimeOfThisMonth() {
+        return new DateTime()
+                .withDayOfMonth(new DateTime().dayOfMonth().getMinimumValue())
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    //获取本月最后一天的最后时间
+    public static synchronized long getLastTimeOfThisMonth() {
+        return new DateTime()
+                .withDayOfMonth(new DateTime().dayOfMonth().getMaximumValue())
+                .withHourOfDay(23)
+                .withMinuteOfHour(59)
+                .withSecondOfMinute(59)
+                .withMillisOfSecond(999)
+                .getMillis();
+    }
+
+    //获取本周的第一天0：00(周一)
+    public static synchronized long getFirstTimeOfThisWeek() {
+        return new DateTime()
+                .withWeekyear(new DateTime().getYear())
+                .withWeekOfWeekyear(new DateTime().getWeekOfWeekyear())
+                .withDayOfWeek(DateTimeConstants.MONDAY)
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    //获取本周的最后一天的23:59:59:999
+    public static synchronized long getLastTimeOfThisWeek() {
+        return new DateTime()
+                .withWeekyear(new DateTime().getYear())
+                .withWeekOfWeekyear(new DateTime().getWeekOfWeekyear())
+                .withDayOfWeek(DateTimeConstants.SUNDAY)
+                .withHourOfDay(23)
+                .withMinuteOfHour(59)
+                .withSecondOfMinute(59)
+                .withMillisOfSecond(999)
+                .getMillis();
+    }
+
+    //获取某月第一天——我不信还有哪个月能给2号起步
+    public static synchronized int getFirstDayOfMonth(int year, int month) {
+        DateTime dateTime = new DateTime(year, month, 1, 0, 0);
+        return dateTime.dayOfMonth().getMinimumValue();
+    }
+    //获取某月的最后一天
+    public static synchronized int getLastDayOfMonth(int year, int month) {
+        DateTime dateTime = new DateTime(year, month, 1, 0, 0);
+        return dateTime.dayOfMonth().getMaximumValue();
+    }
+
+    //获取某一周的第一天0：00
+    public static synchronized long getFirstTimeOfWeek(int year, int weekOfYear) {
+        return new DateTime()
+                .withWeekyear(year)
+                .withWeekOfWeekyear(weekOfYear)
+                .withDayOfWeek(DateTimeConstants.MONDAY)
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    //获取某一周最后一天的最后时间
+    public static synchronized long getLastTimeOfWeek(int year, int weekOfYear) {
+        return new DateTime()
+                .withWeekyear(year)
+                .withWeekOfWeekyear(weekOfYear)
+                .withDayOfWeek(DateTimeConstants.SUNDAY)
+                .withHourOfDay(23)
+                .withMinuteOfHour(59)
+                .withSecondOfMinute(59)
+                .withMillisOfSecond(999)
+                .getMillis();
+    }
+
+    //获取某一年第一天时间
+    public static synchronized long getFirstTimeOfYear(int year) {
+        return new DateTime(year, 1, 1, 0, 0, 0, 0).getMillis();
+    }
+
+    //获取某一年最后一天时间
+    public static synchronized long getLastTimeOfYear(int year) {
+        return new DateTime(year, 12, 31, 23, 59, 59, 999).getMillis();
+
+    }
+
+    //获取某一天的开始/结束时间——我不信有一天能有比24小时多的少的，至少电脑上看来是这样
+    //事实证明我错了，拼字符串太累了！
+    public static synchronized long getFirstTimeOfDay(int year, int month, int day) {
+        return new DateTime()
+                .withYear(year)
+                .withMonthOfYear(month)
+                .withDayOfMonth(day)
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getLastTimeOfDay(int year,int month,int day){
+        return new DateTime()
+                .withYear(year)
+                .withMonthOfYear(month)
+                .withDayOfMonth(day)
+                .withHourOfDay(23)
+                .withMinuteOfHour(59)
+                .withSecondOfMinute(59)
+                .withMillisOfSecond(999)
+                .getMillis();
+    }
+}
