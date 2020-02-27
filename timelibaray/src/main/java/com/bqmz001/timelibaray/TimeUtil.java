f3467b5cdd3ef676f5523b6a56c0f56a1afc4cd2
bqmz001 <hesenrun919359170@outlook.com>
2020-02-27 21:07:50 +0800

    完善

--- timelibaray/src/main/java/com/bqmz001/timelibaray/TimeUtil.java
+++ timelibaray/src/main/java/com/bqmz001/timelibaray/TimeUtil.java
@@ -4,22 +4,20 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
 
 public class TimeUtil {
-    //获取今年
-    public static synchronized int getNowYear() {
-        return new DateTime().getYear();
+
+
+
+    /*
+    当前时间
+     */
+
+
+    //获取当前时间长整型
+    public static synchronized long getNow() {
+        return new DateTime().getMillis();
     }
 
-    //获取本月
-    public static synchronized int getNowMonth() {
-        return new DateTime().getMonthOfYear();
-    }
-
-    //获取今天
-    public static synchronized int getNowDay() {
-        return new DateTime().getDayOfMonth();
-    }
-
-    //获取今天的0：00的字符串
+    //获取今天的0：00长整型
     public static synchronized long getNowDayStartTimeStamp() {
         return new DateTime()
                 .withHourOfDay(0)
@@ -29,7 +27,7 @@ public class TimeUtil {
                 .getMillis();
     }
 
-    //获取今天的23：59：59.999的字符串
+    //获取今天的23：59：59.999长整型
     public static synchronized long getNowDayEndTimeStamp() {
         return new DateTime()
                 .withHourOfDay(23)
@@ -39,34 +37,47 @@ public class TimeUtil {
                 .getMillis();
     }
 
-    //获取当前时间
-    public static synchronized long getNow() {
-        return new DateTime().getMillis();
-    }
-
-    //获取本月第一天0时
-    public static synchronized long getFirstTimeOfThisMonth() {
+    //获取今天任意时间长整型
+    public static synchronized long getNowDayTimeStamp(int hour) {
         return new DateTime()
-                .withDayOfMonth(new DateTime().dayOfMonth().getMinimumValue())
-                .withHourOfDay(0)
+                .withHourOfDay(hour)
                 .withMinuteOfHour(0)
                 .withSecondOfMinute(0)
                 .withMillisOfSecond(0)
                 .getMillis();
     }
 
-    //获取本月最后一天的最后时间
-    public static synchronized long getLastTimeOfThisMonth() {
+    public static synchronized long getNowDayTimeStamp(int hour, int minute) {
         return new DateTime()
-                .withDayOfMonth(new DateTime().dayOfMonth().getMaximumValue())
-                .withHourOfDay(23)
-                .withMinuteOfHour(59)
-                .withSecondOfMinute(59)
-                .withMillisOfSecond(999)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
                 .getMillis();
     }
 
-    //获取本周的第一天0：00(周一)
+    public static synchronized long getNowDayTimeStamp(int hour, int minute, int second) {
+        return new DateTime()
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getNowDayTimeStamp(int hour, int minute, int second, int millSecond) {
+        return new DateTime()
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(millSecond)
+                .getMillis();
+    }
+
+    /*
+       当前周
+     */
+    //获取本周的第一天0：00长整型
     public static synchronized long getFirstTimeOfThisWeek() {
         return new DateTime()
                 .withWeekyear(new DateTime().getYear())
@@ -79,7 +90,7 @@ public class TimeUtil {
                 .getMillis();
     }
 
-    //获取本周的最后一天的23:59:59:999
+    //获取本周的最后一天的23:59:59:999长整型
     public static synchronized long getLastTimeOfThisWeek() {
         return new DateTime()
                 .withWeekyear(new DateTime().getYear())
@@ -92,16 +103,277 @@ public class TimeUtil {
                 .getMillis();
     }
 
-    //获取某月第一天——我不信还有哪个月能给2号起步
-    public static synchronized int getFirstDayOfMonth(int year, int month) {
-        DateTime dateTime = new DateTime(year, month, 1, 0, 0);
-        return dateTime.dayOfMonth().getMinimumValue();
+    //获取本周任意时间长整型
+    public static synchronized long getAnyTimeOfThisWeek(int dayOfWeek) {
+        return new DateTime()
+                .withWeekyear(new DateTime().getYear())
+                .withWeekOfWeekyear(new DateTime().getWeekOfWeekyear())
+                .withDayOfWeek(dayOfWeek)
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
     }
-    //获取某月的最后一天
-    public static synchronized int getLastDayOfMonth(int year, int month) {
-        DateTime dateTime = new DateTime(year, month, 1, 0, 0);
-        return dateTime.dayOfMonth().getMaximumValue();
+
+    public static synchronized long getAnyTimeOfThisWeek(int dayOfWeek, int hour) {
+        return new DateTime()
+                .withWeekyear(new DateTime().getYear())
+                .withWeekOfWeekyear(new DateTime().getWeekOfWeekyear())
+                .withDayOfWeek(dayOfWeek)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
     }
+
+    public static synchronized long getAnyTimeOfThisWeek(int dayOfWeek, int hour, int minute) {
+        return new DateTime()
+                .withWeekyear(new DateTime().getYear())
+                .withWeekOfWeekyear(new DateTime().getWeekOfWeekyear())
+                .withDayOfWeek(dayOfWeek)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyTimeOfThisWeek(int dayOfWeek, int hour, int minute, int second) {
+        return new DateTime()
+                .withWeekyear(new DateTime().getYear())
+                .withWeekOfWeekyear(new DateTime().getWeekOfWeekyear())
+                .withDayOfWeek(dayOfWeek)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyTimeOfThisWeek(int dayOfWeek, int hour, int minute, int second, int millSecond) {
+        return new DateTime()
+                .withWeekyear(new DateTime().getYear())
+                .withWeekOfWeekyear(new DateTime().getWeekOfWeekyear())
+                .withDayOfWeek(dayOfWeek)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(millSecond)
+                .getMillis();
+    }
+
+
+    /*
+   当前月
+    */
+    //获取本月第一天0时长整型
+    public static synchronized long getFirstTimeOfThisMonth() {
+        return new DateTime()
+                .withDayOfMonth(new DateTime().dayOfMonth().getMinimumValue())
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    //获取本月最后一天的最后时间长整型
+    public static synchronized long getLastTimeOfThisMonth() {
+        return new DateTime()
+                .withDayOfMonth(new DateTime().dayOfMonth().getMaximumValue())
+                .withHourOfDay(23)
+                .withMinuteOfHour(59)
+                .withSecondOfMinute(59)
+                .withMillisOfSecond(999)
+                .getMillis();
+    }
+
+    //获取本月任意一天长整型
+    public static synchronized long getAnyTimeOfThisMonth(int dayOfMonth) {
+        return new DateTime()
+                .withDayOfMonth(dayOfMonth)
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyTimeOfThisMonth(int dayOfMonth, int hour) {
+        return new DateTime()
+                .withDayOfMonth(dayOfMonth)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyTimeOfThisMonth(int dayOfMonth, int hour, int minute) {
+        return new DateTime()
+                .withDayOfMonth(dayOfMonth)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyTimeOfThisMonth(int dayOfMonth, int hour, int minute, int second) {
+        return new DateTime()
+                .withDayOfMonth(dayOfMonth)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyTimeOfThisMonth(int dayOfMonth, int hour, int minute, int second, int millSecond) {
+        return new DateTime()
+                .withDayOfMonth(dayOfMonth)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(millSecond)
+                .getMillis();
+    }
+
+    /*
+   当前年
+    */
+    //获取本年第一天0时
+    public static synchronized long getFirstTimeOfThisYear() {
+        return new DateTime()
+                .withMonthOfYear(1)
+                .withDayOfMonth(1)
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    //获取本年最后一天最后一刻
+    public static synchronized long getLastTimeOfThisYear() {
+        return new DateTime()
+                .withMonthOfYear(12)
+                .withDayOfMonth(31)
+                .withHourOfDay(23)
+                .withMinuteOfHour(59)
+                .withSecondOfMinute(59)
+                .withMillisOfSecond(999)
+                .getMillis();
+    }
+
+    //获取本年任何一天的任何时刻
+    public static synchronized long getAnyDayTimeOfThisYear(int dayOfYear) {
+        return new DateTime()
+                .withDayOfYear(dayOfYear)
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyDayTimeOfThisYear(int dayOfYear, int hour) {
+        return new DateTime()
+                .withDayOfYear(dayOfYear)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyDayTimeOfThisYear(int dayOfYear, int hour, int minute) {
+        return new DateTime()
+                .withDayOfYear(dayOfYear)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyDayTimeOfThisYear(int dayOfYear, int hour, int minute, int second) {
+        return new DateTime()
+                .withDayOfYear(dayOfYear)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyDayTimeOfThisYear(int dayOfYear, int hour, int minute, int second, int millSecond) {
+        return new DateTime()
+                .withDayOfYear(dayOfYear)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(millSecond)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyTimeOfThisYear(int month, int day) {
+        return new DateTime()
+                .withMonthOfYear(month)
+                .withDayOfMonth(day)
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyTimeOfThisYear(int month, int day, int hour) {
+        return new DateTime()
+                .withMonthOfYear(month)
+                .withDayOfMonth(day)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyTimeOfThisYear(int month, int day, int hour, int minute) {
+        return new DateTime()
+                .withMonthOfYear(month)
+                .withDayOfMonth(day)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyTimeOfThisYear(int month, int day, int hour, int minute, int second) {
+        return new DateTime()
+                .withMonthOfYear(month)
+                .withDayOfMonth(day)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+
+    public static synchronized long getAnyTimeOfThisYear(int month, int day, int hour, int minute, int second, int millSecond) {
+        return new DateTime()
+                .withMonthOfYear(month)
+                .withDayOfMonth(day)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(millSecond)
+                .getMillis();
+    }
+
 
     //获取某一周的第一天0：00
     public static synchronized long getFirstTimeOfWeek(int year, int weekOfYear) {
@@ -128,6 +400,64 @@ public class TimeUtil {
                 .withMillisOfSecond(999)
                 .getMillis();
     }
+    
+    //获取某一周中的任意时间
+    public static synchronized long getAnyTimeOfWeek(int year, int weekOfYear,int dayOfWeek) {
+        return new DateTime()
+                .withWeekyear(year)
+                .withWeekOfWeekyear(weekOfYear)
+                .withDayOfWeek(dayOfWeek)
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+    public static synchronized long getAnyTimeOfWeek(int year, int weekOfYear,int dayOfWeek,int hour) {
+        return new DateTime()
+                .withWeekyear(year)
+                .withWeekOfWeekyear(weekOfYear)
+                .withDayOfWeek(dayOfWeek)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+    public static synchronized long getAnyTimeOfWeek(int year, int weekOfYear,int dayOfWeek,int hour,int minute) {
+        return new DateTime()
+                .withWeekyear(year)
+                .withWeekOfWeekyear(weekOfYear)
+                .withDayOfWeek(dayOfWeek)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+    public static synchronized long getAnyTimeOfWeek(int year, int weekOfYear,int dayOfWeek,int hour,int minute,int second) {
+        return new DateTime()
+                .withWeekyear(year)
+                .withWeekOfWeekyear(weekOfYear)
+                .withDayOfWeek(dayOfWeek)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(0)
+                .getMillis();
+    }
+    public static synchronized long getAnyTimeOfWeek(int year, int weekOfYear,int dayOfWeek,int hour,int minute,int second,int millSecond) {
+        return new DateTime()
+                .withWeekyear(year)
+                .withWeekOfWeekyear(weekOfYear)
+                .withDayOfWeek(dayOfWeek)
+                .withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(second)
+                .withMillisOfSecond(millSecond)
+                .getMillis();
+    }
+
 
     //获取某一年第一天时间
     public static synchronized long getFirstTimeOfYear(int year) {
@@ -137,6 +467,28 @@ public class TimeUtil {
     //获取某一年最后一天时间
     public static synchronized long getLastTimeOfYear(int year) {
         return new DateTime(year, 12, 31, 23, 59, 59, 999).getMillis();
+
+    }
+
+    //获取某一年任何一天的时间
+    public static synchronized long getAnyDayOfYear(int year,int month,int day) {
+        return new DateTime(year, month, day, 0, 0, 0, 0).getMillis();
+
+    }
+    public static synchronized long getAnyDayOfYear(int year,int month,int day,int hour) {
+        return new DateTime(year, month, day, hour, 0, 0, 0).getMillis();
+
+    }
+    public static synchronized long getAnyDayOfYear(int year,int month,int day,int hour,int minute) {
+        return new DateTime(year, month, day, hour, minute, 0, 0).getMillis();
+
+    }
+    public static synchronized long getAnyDayOfYear(int year,int month,int day,int hour,int minute,int second) {
+        return new DateTime(year, month, day, hour, minute, second, 0).getMillis();
+
+    }
+    public static synchronized long getAnyDayOfYear(int year,int month,int day,int hour,int minute,int second,int millSecond) {
+        return new DateTime(year, month, day, hour, minute, second, millSecond).getMillis();
 
     }
 
@@ -154,7 +506,7 @@ public class TimeUtil {
                 .getMillis();
     }
 
-    public static synchronized long getLastTimeOfDay(int year,int month,int day){
+    public static synchronized long getLastTimeOfDay(int year, int month, int day) {
         return new DateTime()
                 .withYear(year)
                 .withMonthOfYear(month)
@@ -164,5 +516,33 @@ public class TimeUtil {
                 .withSecondOfMinute(59)
                 .withMillisOfSecond(999)
                 .getMillis();
+    }
+
+
+    //获取今年
+    public static synchronized int getNowYear() {
+        return new DateTime().getYear();
+    }
+
+    //获取本月
+    public static synchronized int getNowMonth() {
+        return new DateTime().getMonthOfYear();
+    }
+
+    //获取今天
+    public static synchronized int getNowDay() {
+        return new DateTime().getDayOfMonth();
+    }
+
+    //获取某月第一天——我不信还有哪个月能给2号起步——算了，写上吧，总有人要给你出幺蛾子
+    public static synchronized int getFirstDayOfMonth(int year, int month) {
+        DateTime dateTime = new DateTime(year, month, 1, 0, 0);
+        return dateTime.dayOfMonth().getMinimumValue();
+    }
+
+    //获取某月的最后一天
+    public static synchronized int getLastDayOfMonth(int year, int month) {
+        DateTime dateTime = new DateTime(year, month, 1, 0, 0);
+        return dateTime.dayOfMonth().getMaximumValue();
     }
 }
